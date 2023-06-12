b3e2be2cc3dfdae425084e9e8b8bacf5b8f5dbbc
Matheus Torres <108310425+jsxtheuz@users.noreply.github.com>
2023-06-12 10:56:14 -0300

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,21 @@
+function ingles(){
+
+    const portugues  = document.getElementById("portugues");
+    const ingless  = document.getElementById("ingles");
+    const trabalho = document.getElementById("work");
+ 
+    portugues.innerText = "Portuguese";
+    ingless.innerText= "English";
+    trabalho.innerText = "Junior Developer";
+ }
+ 
+ function ptbr(){
+ 
+    const portugues  = document.getElementById("portugues");
+    const ingless  = document.getElementById("ingles");
+    const trabalho = document.getElementById("work");
+    
+    portugues.innerText = "Português";
+    ingless.innerText= "Inglês";
+    trabalho.innerText = "Desenvolvedor Júnior";
+ }
